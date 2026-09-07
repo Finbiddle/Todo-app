597cfdd729af227f1e6d9498fb4cea92a3eb70b5
Tomi Koskinen <finbiddle@gmail.com>
2026-09-07 14:34:21 +0300

    initial drift

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Authentication, { AuthenticationMode } from './screens/Authentication'
+import ProtectedRoute from './components/ProtectedRoute'
+import UserProvider from './context/UserProvider'
+import NotFound from './screens/NotFound'
+import './index.css'
+
+const router = createBrowserRouter([
+    {
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "/signin",
+                element: <Authentication authenticationMode={AuthenticationMode.SignIn} />
+            },
+            {
+                path: "/signup",
+                element: <Authentication authenticationMode={AuthenticationMode.SignUp} />
+            },
+            {
+                element: <ProtectedRoute />,
+                children: [
+                    {
+                        path: "/",
+                        element: <App />
+                    }
+                ]
+            }
+        ]
+    }
+])
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <UserProvider>
+            <RouterProvider router={router} />
+        </UserProvider>
+    </StrictMode>
+)
